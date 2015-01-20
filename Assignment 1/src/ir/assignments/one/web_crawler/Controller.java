1f98fa2bf8de476be95f497a7d3fbec717395366
jfeng1115 <jfeng1115@gmail.com>
2015-01-19 22:19:04 -0500

    fixed log4j.properties problem. use project structure to set class path

--- Assignment 1/src/ir/assignments/one/web_crawler/Controller.java
+++ Assignment 1/src/ir/assignments/one/web_crawler/Controller.java
@@ -13,8 +13,8 @@ import edu.uci.ics.crawler4j.url.WebURL;
 
     public class Controller {
         public static void main(String[] args) throws Exception {
-            String crawlStorageFolder = "/data/crawl/root";
-            int numberOfCrawlers = 7;
+            String crawlStorageFolder = "/Users/jiefeng/Dropbox/programming/Java/Information-Retrieval/data/crawl/root";
+            int numberOfCrawlers = 3;
 
             CrawlConfig config = new CrawlConfig();
             config.setCrawlStorageFolder(crawlStorageFolder);
@@ -25,22 +25,29 @@ import edu.uci.ics.crawler4j.url.WebURL;
             PageFetcher pageFetcher = new PageFetcher(config);
             RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
             RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-            CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-
+            CrawlController controller;
+            try {
+                controller = new CrawlController(config, pageFetcher, robotstxtServer);
                 /*
                  * For each crawl, you need to add some seed urls. These are the first
                  * URLs that are fetched and then the crawler starts following links
                  * which are found in these pages
                  */
-            controller.addSeed("http://www.ics.uci.edu/~welling/");
-            controller.addSeed("http://www.ics.uci.edu/~lopes/");
-            controller.addSeed("http://www.ics.uci.edu/");
+                controller.addSeed("http://www.ics.uci.edu/~welling/");
+                controller.addSeed("http://www.ics.uci.edu/~lopes/");
+                controller.addSeed("http://www.ics.uci.edu/");
 
                 /*
                  * Start the crawl. This is a blocking operation, meaning that your code
                  * will reach the line after this only when crawling is finished.
                  */
-            controller.start(MyCrawler.class, numberOfCrawlers);
+                controller.start(MyCrawler.class, numberOfCrawlers);
+            }catch(Exception e){
+                System.out.println("Exception"+e);
+            }
+
+
+
         }
     }
 
